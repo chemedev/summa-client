77874a15748ee7c28669f239a5893db39ac935cf
Juan Chemello <juanchemell@gmail.com>
2021-01-06 20:50:41 -0300

    Initialized

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import './App.scss'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Summa Solutions 💼</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
