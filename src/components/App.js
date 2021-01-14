8fdab70d29b683999750ae1ab8f2af7652c805a8
Juan Chemello <juanchemell@gmail.com>
2021-01-14 10:06:45 -0300

    Feature: List and filter components

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,48 @@
 import { useEffect, useState } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
+import List from './List'
 import Header from './Header'
 
 function App() {
   const [error, setError] = useState('')
+  const [employees, setEmployees] = useState([])
+  const [ageAverage, setAgeAverage] = useState(0)
+
+  useEffect(() => {
+    const fetchEmployees = async () => {
+      const url = 'http://localhost:3001/employees?'
+      const { data, error, msg } = await (await fetch(url)).json()
+      if (error) setError(msg)
+      else {
+        setError('')
+        setEmployees(data.employees)
+        setAgeAverage(data.ageAverage)
+      }
+    }
+    fetchEmployees()
+  }, [])
 
   return (
     <BrowserRouter>
       <div className="App">
         <Header error={error} />
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={props => (
+              <List
+                employees={employees}
+                setError={setError}
+                setEmployees={setEmployees}
+                setAgeAverage={setAgeAverage}
+                averageAge={ageAverage}
+                {...props}
+              />
+            )}
+          />
+        </Switch>
       </div>
     </BrowserRouter>
   )
---
+++ src/components/Filter.js
@@ -0,0 +1,122 @@
+import { useState, useEffect } from 'react'
+
+const Filter = ({ setError, setEmployees, setAgeAverage }) => {
+  const [option, setOption] = useState(0)
+  const [select, setSelect] = useState('Todos')
+  const [search, setSearch] = useState(false)
+  const [roles, setRoles] = useState([])
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    setSearch(true)
+  }
+
+  const handleChange = value => {
+    setOption(value)
+    setSelect('Todos')
+  }
+
+  useEffect(() => {
+    const fetchEmployees = async () => {
+      let url = 'http://localhost:3001/employees?'
+      if (option !== 0) {
+        const query = 'roleId'
+        url += `${query}=${option}`
+      }
+      if (select !== 'Todos') {
+        let query
+        if (option === 1) query = 'designerTypeId'
+        else query = 'programmingLanguageId'
+
+        url += `&${query}=${select}`
+      }
+
+      const { data, error, msg } = await (await fetch(url)).json()
+
+      if (error) setError(msg)
+      else {
+        setError('')
+        setEmployees(data.employees)
+        setAgeAverage(data.ageAverage)
+      }
+    }
+
+    if (search) {
+      fetchEmployees()
+      setSearch(false)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [search])
+
+  useEffect(() => {
+    const fetchRoles = async () => {
+      let role = option === 1 ? 'designer' : 'developer'
+      let url = `http://localhost:3001/roles/${role}`
+      const { data } = await (await fetch(url)).json()
+      setRoles(data)
+    }
+
+    if (option !== 0) {
+      fetchRoles()
+    }
+  }, [option])
+
+  return (
+    <form className="form-Filter" onSubmit={handleSubmit}>
+      <div className="form-Filter__container">
+        <input
+          hidden
+          id="all"
+          type="radio"
+          checked={option === 0}
+          name="role"
+          onChange={() => handleChange(0)}
+        />
+        <label className="btn form-Filter__radio" htmlFor="all">
+          👥 Todos
+        </label>
+        <input
+          hidden
+          id="designer"
+          type="radio"
+          name="role"
+          checked={option === 1}
+          onChange={() => handleChange(1)}
+        />
+        <label className="btn form-Filter__radio" htmlFor="designer">
+          🎨 Diseñadores
+        </label>
+        <input
+          hidden
+          id="developer"
+          type="radio"
+          name="role"
+          checked={option === 2}
+          onChange={() => handleChange(2)}
+        />
+        <label className="btn form-Filter__radio" htmlFor="developer">
+          👨‍💻 Desarrolladores
+        </label>
+      </div>
+      <div className="form-Filter__container">
+        <select
+          className="btn select"
+          value={select}
+          onChange={e => setSelect(e.target.value)}
+          disabled={!option}
+        >
+          <option value="Todos">Todos</option>
+          {roles &&
+            roles.map(role => (
+              <option value={role.id} key={role.id}>
+                {role.name}
+              </option>
+            ))}
+        </select>
+        <input className="btn submit" type="submit" value="Filtrar" />
+      </div>
+    </form>
+  )
+}
+
+export default Filter
